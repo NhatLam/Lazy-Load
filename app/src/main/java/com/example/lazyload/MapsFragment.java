18b694aa4ad8d5e0aed5e9dd8472a659f3bd4093
NhatLam <31428056+NhatLam@users.noreply.github.com>
2019-06-20 15:03:46 +0700

    Update MapsFragment.java

--- app/src/main/java/com/example/lazyload/MapsFragment.java
+++ app/src/main/java/com/example/lazyload/MapsFragment.java
@@ -3,8 +3,10 @@ package com.example.lazyload;
 
 import android.arch.lifecycle.ViewModelProviders;
 
+import android.arch.paging.PagedList;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +16,14 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 
 public class MapsFragment extends Fragment   {
     GoogleMap map;
     MapView mMapView;
     ResultViewModel viewModel;
-    public MapsFragment() {
 
-    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_maps, container, false);
@@ -30,27 +32,33 @@ public class MapsFragment extends Fragment   {
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
         viewModel = ViewModelProviders.of(getActivity()).get(ResultViewModel.class);
-        viewModel.itemPagedList.observe(getActivity(),  items -> {
-            mMapView.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap googleMap) {
-                    map = googleMap;
-                    for (int i = 0; i < items.size(); i++) {
-                        map.addMarker(new MarkerOptions().title(items.get(i).getId())
-                                .snippet(items.get(i).getDyadName())
-                                .position(new LatLng(Double.parseDouble(items.get(i).getLatitude()), Double.parseDouble(items.get(i).getLongitude()))));
-                    }
-                }
-            });
-
-
-
-
-
-        });
-
 
         return rootView;
+    }
+
+    public void setMapview(PagedList<DetailInfo> items){
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                for (int i = 0; i < items.size(); i++) {
+                    map.addMarker(new MarkerOptions().title(items.get(i).getId())
+                            .snippet(items.get(i).getDyadName())
+                            .position(new LatLng(Double.parseDouble(items.get(i).getLatitude()), Double.parseDouble(items.get(i).getLongitude()))));
+                }
+            }
+        });
+
+    }
+
+    public void reloadMapview(){
+        mMapView.invalidate();
+
+        viewModel.itemPagedList.observe(getActivity(),  items -> {
+            if (!items.isEmpty()){
+                setMapview(items);
+            }
+        });
     }
 
 

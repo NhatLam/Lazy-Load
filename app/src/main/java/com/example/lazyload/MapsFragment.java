2e2c52f7c19c07ea394fdd824a36595bbe35b570
NhatLam <31428056+NhatLam@users.noreply.github.com>
2019-06-20 15:55:05 +0700

    Update MapsFragment.java

--- app/src/main/java/com/example/lazyload/MapsFragment.java
+++ app/src/main/java/com/example/lazyload/MapsFragment.java
@@ -4,25 +4,34 @@ package com.example.lazyload;
 import android.arch.lifecycle.ViewModelProviders;
 
 import android.arch.paging.PagedList;
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Person;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.Cluster;
+import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 
 import java.util.ArrayList;
 
 
-public class MapsFragment extends Fragment   {
+public class MapsFragment extends Fragment {
     GoogleMap map;
     MapView mMapView;
     ResultViewModel viewModel;
+    ClusterManager<MyCluster> mClusterManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,29 +42,39 @@ public class MapsFragment extends Fragment   {
         mMapView.onResume();
         viewModel = ViewModelProviders.of(getActivity()).get(ResultViewModel.class);
 
+
         return rootView;
     }
 
-    public void setMapview(PagedList<DetailInfo> items){
+    public void setMapview(PagedList<DetailInfo> items) {
+
         mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 map = googleMap;
+                mClusterManager = new ClusterManager(getActivity(), map);
+                map.setOnCameraIdleListener(mClusterManager);
+                map.setOnMarkerClickListener(mClusterManager);
                 for (int i = 0; i < items.size(); i++) {
-                    map.addMarker(new MarkerOptions().title(items.get(i).getId())
-                            .snippet(items.get(i).getDyadName())
-                            .position(new LatLng(Double.parseDouble(items.get(i).getLatitude()), Double.parseDouble(items.get(i).getLongitude()))));
+
+                    MyCluster offsetItem = new MyCluster(Double.parseDouble(items.get(i).getLatitude()), Double.parseDouble((items.get(i).getLongitude())), items.get(i).getId(), items.get(i).getDyadName());
+                    mClusterManager.addItem(offsetItem);
+                    mClusterManager.cluster();
+
                 }
+
+
             }
         });
 
+
     }
 
-    public void reloadMapview(){
+    public void reloadMapview() {
         mMapView.invalidate();
 
-        viewModel.itemPagedList.observe(getActivity(),  items -> {
-            if (!items.isEmpty()){
+        viewModel.itemPagedList.observe(getActivity(), items -> {
+            if (!items.isEmpty()) {
                 setMapview(items);
             }
         });

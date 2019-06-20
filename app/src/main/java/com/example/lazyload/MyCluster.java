f50888427c6018a6b87a54c37ab4fe81e759b68a
NhatLam <31428056+NhatLam@users.noreply.github.com>
2019-06-20 15:55:29 +0700

    Create MyCluster.java

---
+++ app/src/main/java/com/example/lazyload/MyCluster.java
@@ -0,0 +1,33 @@
+package com.example.lazyload;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+public class MyCluster implements ClusterItem {
+
+
+    private  LatLng mPosition;
+    private  String mTitle;
+    private  String mSnippet;
+
+    public MyCluster(double lat, double lng, String title, String snippet) {
+        mPosition = new LatLng(lat, lng);
+        mTitle = title;
+        mSnippet = snippet;    }
+
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+
+    @Override
+    public String getTitle() {
+        return mTitle;
+    }
+
+    @Override
+    public String getSnippet() {
+        return mSnippet;
+    }
+}
